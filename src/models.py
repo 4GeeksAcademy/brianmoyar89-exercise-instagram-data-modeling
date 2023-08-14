81a5e5dc6e846164698323ba1d6b43178a2cc27d
Brian Moya <122998981+brianmoyar89@users.noreply.github.com>
2023-08-14 01:48:32 +0000

    terminado

--- src/models.py
+++ src/models.py
@@ -1,37 +1,52 @@
-import os
-import sys
-from sqlalchemy import Column, ForeignKey, Integer, String
-from sqlalchemy.orm import relationship, declarative_base
+from sqlalchemy import Column, ForeignKey, Integer, String, DateTime
+from sqlalchemy.orm import relationship
+from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import create_engine
 from eralchemy2 import render_er
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
-    # Here we define columns for the table person
-    # Notice that each column is also a normal Python instance attribute.
+class User(Base):
+    __tablename__ = 'user'
     id = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable=False)
+    username = Column(String(50), unique=True, nullable=False)
+    full_name = Column(String(100))
+    posts = relationship('Post', back_populates='user')
 
-class Address(Base):
-    __tablename__ = 'address'
-    # Here we define columns for the table address.
-    # Notice that each column is also a normal Python instance attribute.
+class Post(Base):
+    __tablename__ = 'post'
     id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+    caption = Column(String(280))
+    created_at = Column(DateTime, nullable=False)
+    user_id = Column(Integer, ForeignKey('user.id'))
+    user = relationship('User', back_populates='posts')
+    comments = relationship('Comment', back_populates='post')
+    media = relationship('Media', back_populates='post')
 
-    def to_dict(self):
-        return {}
+class Comment(Base):
+    __tablename__ = 'comment'
+    id = Column(Integer, primary_key=True)
+    text = Column(String(280), nullable=False)
+    post_id = Column(Integer, ForeignKey('post.id'))
+    post = relationship('Post', back_populates='comments')
 
-## Draw from SQLAlchemy base
+class Media(Base):
+    __tablename__ = 'media'
+    id = Column(Integer, primary_key=True)
+    url = Column(String(255), nullable=False)
+    post_id = Column(Integer, ForeignKey('post.id'))
+    post = relationship('Post', back_populates='media')
+
+# Replace 'sqlite:///instagram.db' with your preferred database connection string
+engine = create_engine('sqlite:///instagram.db')
+Base.metadata.create_all(engine)
+
 try:
     result = render_er(Base, 'diagram.png')
     print("Success! Check the diagram.png file")
 except Exception as e:
-    print("There was a problem genering the diagram")
+    print("There was a problem generating the diagram")
     raise e
+
+
+
